fae5c0388ec35c0a18332a6159bb0faa1e830c14
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-08 17:42:17 +0400

    Create Power2DArray.java

---
+++ Power2DArray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Power2DArray {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int k = 1;
+
+        int[][] array = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                array[i][j] = k;
+                k++;
+                System.out.print(array[i][j] + " ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
