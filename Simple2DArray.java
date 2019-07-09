8addc18241cd97934d3934fae02cd1db48945552
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-09 18:45:57 +0400

    Create Simple2DArray.java

---
+++ Simple2DArray.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Simple2DArray{
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter Size of Array:");
+
+        int n = scan.nextInt();
+
+        int[][] array = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+
+            int k = 0;
+
+            for (int j = 0; j < n; j++) {
+
+                array[i][j] = k;
+
+                k++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                System.out.print(array[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+
+    }
+}
