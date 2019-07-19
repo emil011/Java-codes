8c2513c35ff6f4710e19db9f02176f1b3391de79
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-19 14:28:53 +0400

    Update DeleteElementOfIndexFromArray.java

--- DeleteElementOfIndexFromArray.java
+++ DeleteElementOfIndexFromArray.java
@@ -2,8 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class DeleteElementOfIndexFromArray{
-
-
+    
     public static void main(String[] args) {
 
         Random rd = new Random();
@@ -20,34 +19,26 @@ public class DeleteElementOfIndexFromArray{
 
             arr[i] = rd.nextInt(50);
 
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + " ");
 
         }
+        System.out.println();
 
         System.out.print("Remove element of index: ");
 
         int query = scan.nextInt();
 
+        for (int i = query; i < arr.length - 1; i++) {
 
-        for (int i = 0; i < arr.length; i++) {
-
-            if(i == query){
-
-                for(int j = i; j < arr.length - 1; j++) {
-
-                    arr[j] = arr[j + 1];
-                }
-
-            }
+            arr[i] = arr[i + 1];
         }
 
         for(int i = 0; i < arr.length - 1; i++){
 
-            System.out.print(" " + arr[i]);
+            System.out.print(arr[i] + " ");
 
         }
-
-
+        
     }
 
 }
