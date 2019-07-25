256dd4d4f6a19d50dc8065888b44cd61f273b26f
Emil Karim <32624787+emil011@users.noreply.github.com>
2019-07-25 10:08:20 +0400

    Update SecondLargestNumberOfArray.java

--- SecondLargestNumberOfArray.java
+++ SecondLargestNumberOfArray.java
@@ -20,29 +20,37 @@ public class SecondLargestNumberOfArray{
 
 
             arr[i] = rd.nextInt(20);
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + " ");
 
         }
+        System.out.println();
 
 
-        int largest = arr[0], secondLargest = arr[0], pos = 0;
+        int largest = arr[0], secondlargest = 0, pos = -1;
 
 
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 1; i < arr.length; i++){
 
-            if (arr[i] > largest) {
-                secondLargest = largest;
+            if(largest < arr[i]){
+
                 largest = arr[i];
 
-            } else if (arr[i] > secondLargest && arr[i] != largest) {
-                secondLargest = arr[i];
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+
+            if(arr[i] != largest && (pos == -1 || arr[i] > secondlargest)){
+
+                secondlargest = arr[i];
                 pos = i;
 
             }
         }
 
 
-        System.out.print("Second largest number is: " + secondLargest + " Position " + pos);
+
+        System.out.print("Second largest number is: " + secondlargest + " Position " + pos);
 
 
     }
